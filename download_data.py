7097e7c6b9b6e825c5ebe261ca2bdfde09faad48
jwcalder <48805965+jwcalder@users.noreply.github.com>
2020-07-17 13:06:40 -0500

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -12,9 +12,9 @@ print('Downloading FashionMNIST vae data...')
 wget.download('http://www-users.math.umn.edu/~jwcalder/FashionMNIST_vae.npz','./Data/FashionMNIST_vae.npz')
 print('\n')
 
-#print('Downloading CIFAR raw data...')
-#wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_raw.npz','./Data/cifar_raw.npz')
-#print('\n')
+print('Downloading CIFAR raw data...')
+wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_raw.npz','./Data/cifar_raw.npz')
+print('\n')
 
 print('Downloading CIFAR aet data...')
 wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_aet.npz','./Data/cifar_raw.npz')
