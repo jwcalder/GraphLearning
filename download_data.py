dcaac2974855d1a6311a371fa4862302c075b0e1
jwcalder <48805965+jwcalder@users.noreply.github.com>
2020-09-23 11:02:18 -0500

    Fixed bug in download_data.py

--- download_data.py
+++ download_data.py
@@ -1,21 +1,33 @@
 import wget
+from os import path
 
-print('Downloading MNIST raw data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/MNIST_raw.npz','./Data/MNIST_raw.npz')
-print('Downloading MNIST vae data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/MNIST_vae.npz','./Data/MNIST_vae.npz')
-print('\n')
+if not path.isfile('./Data/MNIST_raw.npz'):
+    print('Downloading MNIST raw data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/MNIST_raw.npz','./Data/MNIST_raw.npz')
+    print('\n')
 
-print('Downloading FashionMNIST raw data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/FashionMNIST_raw.npz','./Data/FashionMNIST_raw.npz')
-print('Downloading FashionMNIST vae data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/FashionMNIST_vae.npz','./Data/FashionMNIST_vae.npz')
-print('\n')
 
-print('Downloading CIFAR raw data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_raw.npz','./Data/cifar_raw.npz')
-print('\n')
+if not path.isfile('./Data/MNIST_vae.npz'):
+    print('Downloading MNIST vae data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/MNIST_vae.npz','./Data/MNIST_vae.npz')
+    print('\n')
 
-print('Downloading CIFAR aet data...')
-wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_aet.npz','./Data/cifar_raw.npz')
-print('\n')
+if not path.isfile('./Data/FashionMNIST_raw.npz'):
+    print('Downloading FashionMNIST raw data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/FashionMNIST_raw.npz','./Data/FashionMNIST_raw.npz')
+    print('\n')
+
+if not path.isfile('./Data/FashionMNIST_vae.npz'):
+    print('Downloading FashionMNIST vae data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/FashionMNIST_vae.npz','./Data/FashionMNIST_vae.npz')
+    print('\n')
+
+if not path.isfile('./Data/cifar_raw.npz'):
+    print('Downloading CIFAR raw data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_raw.npz','./Data/cifar_raw.npz')
+    print('\n')
+
+if not path.isfile('./Data/cifar_aet.npz'):
+    print('Downloading CIFAR aet data...')
+    wget.download('http://www-users.math.umn.edu/~jwcalder/cifar_aet.npz','./Data/cifar_aet.npz')
+    print('\n')
