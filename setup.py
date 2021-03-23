74e07284710dcd3b7d06c18a903d262586748d7a
Jeff Calder <jcalder@umn.edu>
2021-03-22 22:17:06 -0500

    v2 and fixed typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="graphlearning", 
-    version="0.0.1",
+    version="0.0.2",
     author="Jeff Calder",
     author_email="jwcalder@umn.edu",
     description="Python package for graph-based clustering and semi-supervised learning",
@@ -21,20 +21,21 @@ setuptools.setup(
                                 'src/memory_allocation.c',
                                 'src/mnist_benchmark.c',
                                 'src/mbo_speedy_volume_preserving.c'],
-                    install_requires=[  'numpy', 
-                                        'scipy', 
-                                        'torch', 
-                                        'annoy', 
-                                        'sklearn', 
-                                        'kymatio',
-                                        'matplotlib'],
                     include_dirs=[numpy.get_include()],
                     extra_compile_args = ['-Ofast'],
                     extra_link_args = ['-lm'])],
-                    classifiers=[
-                                "Programming Language :: Python :: 3",
-                                "License :: OSI Approved :: MIT License",
-                                "Operating System :: OS Independent"],
-                    python_requires='>=3.6',
+    classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent"],
+    install_requires=[  'numpy', 
+                        'scipy', 
+                        'torch', 
+                        'annoy', 
+                        'sklearn', 
+                        'kymatio',
+                        'matplotlib'],
+    python_requires='>=3.6',
 )
+
 
