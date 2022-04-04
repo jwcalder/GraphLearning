cd0ba1bc6889ba03552002e1faa522917f11c310
Jeff Calder <jcalder@umn.edu>
2022-04-04 15:36:14 -0500

    Added cone image

--- examples/dijkstra_hl.py
+++ examples/dijkstra_hl.py
@@ -1,11 +1,12 @@
 import graphlearning as gl
 import matplotlib.pyplot as plt
 import numpy as np
+from mayavi import mlab
 
 
 n = 10000
-X = np.random.rand(n,2)
-X[0,:]=[0.5,0.5]
+X = gl.utils.rand_ball(n,2)
+X[0,:]=[0,0]
 W = gl.weightmatrix.knn(X,50,kernel='distance',symmetrize=True)
 G = gl.graph(W)
 u = G.dijkstra_hl([0])
@@ -17,4 +18,9 @@ print('Residual=%f'%np.max(np.absolute(H-u)))
 
 x,y = X[:,0],X[:,1]
 plt.scatter(x,y,c=u)
+Tri = gl.utils.mesh(X)
+mlab.figure(size=(1000,1000),bgcolor=(1,1,1))
+mlab.triangular_mesh(x,y,3*(np.max(u)-u),Tri)
+mlab.savefig('cone.png')
+mlab.show()
 plt.show()
