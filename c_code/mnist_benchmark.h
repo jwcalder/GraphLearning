29b7b5425e6caa66773562783a058bf51c540b57
Jeff Calder <jcalder@umn.edu>
2024-07-09 20:28:43 -0500

    Updates

--- c_code/mnist_benchmark.h
+++ c_code/mnist_benchmark.h
@@ -4,6 +4,6 @@
 #include <assert.h>
 
 
-void mbo_main(int *labels, int *I, int *J, float *W, int *ind, int *val, int *classCounts, bool prog, int pcount, int M, int m, int lcount, int maxIters, float stoppingCriterion, float temperature, float upperVolumeMultiplier, float lowerVolumeMultiplier);
+void mbo_main(int *labels, int *II, int *J, float *W, int *ind, int *val, int *classCounts, bool prog, int pcount, int M, int m, int lcount, int maxIters, float stoppingCriterion, float temperature, float upperVolumeMultiplier, float lowerVolumeMultiplier);
 
 void mbo_main_original(int k, int numTrials, float trainingFraction, int maxIters, float StoppingCriterion, float temperature, float upperVolumeMultiplier, float lowerVolumeMultiplier);
